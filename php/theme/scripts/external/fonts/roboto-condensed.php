ea4d99cdfb5707f22bb7d475b7c4ce2a5ac7fb98
Scott Shealy <vsshealy@gmail.com>
2022-05-19 10:28:54 -0400

    Updated font selections

--- php/theme/scripts/external/fonts/roboto-condensed.php
+++ php/theme/scripts/external/fonts/roboto-condensed.php
@@ -1,6 +1,6 @@
 <?php 
     /**
-     * php/theme/scripts/external/fonts/fira-code.php
+     * php/theme/scripts/external/fonts/roboto-condensed.php
      * @package scgolfpanel
      * @author Scott Shealy
      * @version 1.0.0
@@ -10,4 +10,4 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet"/>
